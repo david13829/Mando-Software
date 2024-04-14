f9abc5644a4b4de3edebbab38432e1e08d886d7b
david13829 <164719642+david13829@users.noreply.github.com>
2024-04-14 21:20:23 +0900

    Delete Tic.h

--- Tic.h
+++
@@ -1,9 +0,0 @@
-class ttt{
-    public:
-        void board();
-        void turn();
-        bool result(char sign);
-    private:
-        char space[3][3] = {{'1','2','3'},{'4','5','6'},{'7','8','9'}};
-        char t = 'X';
-};
