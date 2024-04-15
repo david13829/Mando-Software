854d8368f944a7c40d3d4f2cd0a269216f489dbd
david13829 <164719642+david13829@users.noreply.github.com>
2024-04-15 11:50:00 +0900

    Add files via upload

---
+++ Calculator_class/calculator.h
@@ -0,0 +1,10 @@
+class calculator{
+    public:
+    double addition(double n2);
+    double subtraction(double n2);
+    double multiplication(double n2);
+    double division(double n2);
+    double clear();
+    private:
+    double sum = 0;
+};
